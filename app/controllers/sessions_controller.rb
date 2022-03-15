28554df47d88f8a8fb3278946c8dc67156f45a6c
JakeTSherman <jake.sherman@kellogg.northwestern.edu>
2022-03-15 22:41:00 +0000

    auth login general updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.where({email: params["email"]})[0]
+        if @user
+         if BCrypt::Password.new(@user.password) == params["password"]
+             session["user_id"] = @user.id
+
+             flash[:notice] = "Welcome, #{@user.username}! You logged in!"
+             redirect_to "/"
+         else
+             flash[:notice] = "Nope."
+             redirect_to "/sessions/new"
+         end
+        else
+         flash[:notice] = "Nope."
+         redirect_to "/sessions/new"
+     end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Goodbye."
+    redirect_to "/sessions/new"
   end
 end
   
